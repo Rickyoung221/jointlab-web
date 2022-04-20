948ed99a450eee5a15629193de99fa4fdf3abae3
Weikeng Yang <weikengyang@gmail.com>
2022-04-20 10:17:57 -0700

    实现二级嵌套路由

--- client/src/views/routes/index.jsx
+++ client/src/views/routes/index.jsx
@@ -1,78 +1,79 @@
-import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import '../../assets/index.css';
-import '../../assets/scss/mdb.min.css';
-import '../../assets/scss/custom_variables.scss';
-import Home from '../Home';
-import App from '../../App';
-import Avatar from '../Vision/Avatar';
-import AboutUs from '../Vision/AboutUs';
-import Missions from '../Vision/Mission';
-import Partners from '../Vision/Partners';
-import Advisory from '../Vision/AdvisoryBoard';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "../../assets/index.css";
+import "../../assets/scss/mdb.min.css";
+import "../../assets/scss/custom_variables.scss";
+import Home from "../Home";
+import App from "../../App";
+import Avatar from "../Vision/Avatar";
+import AboutUs from "../Vision/AboutUs";
+import Missions from "../Vision/Mission";
+import Partners from "../Vision/Partners";
+import Advisory from "../Vision/AdvisoryBoard";
 
-import JoinUs from '../JoinUs';
+import JoinUs from "../JoinUs";
 
-import FinishedProjects from '../Research/FinishedProjects';
-import OnGoingProjects from '../Research/OnGoingProjects';
-import EducationDevelopment from '../Research/FinishedProjects/EducationDevelopment';
+import FinishedProjects from "../Research/FinishedProjects";
+import OnGoingProjects from "../Research/OnGoingProjects";
+import EducationDevelopment from "../Research/FinishedProjects/EducationDevelopment";
 
-import Reports from '../Publication/Report';
-import JournalArticles from '../Publication/JournalArticle';
-import Conferences from '../Publication/Conference';
+import Reports from "../Publication/Report";
+import JournalArticles from "../Publication/JournalArticle";
+import Conferences from "../Publication/Conference";
 
-import Workshop from '../Event/Workshop';
-import WorkshopContent from '../Event/Workshop/content/WorkshopContent';
-import Seminar from '../Event/Seminar';
-import MediaCover from '../Event/MediaCover';
-import MediaCoverContent from '../Event/MediaCover/content/MarkdownDisplay';
+import Workshop from "../Event/Workshop";
+import WorkshopContent from "../Event/Workshop/content/WorkshopContent";
+import Seminar from "../Event/Seminar";
+import MediaCover from "../Event/MediaCover";
+import MediaCoverContent from "../Event/MediaCover/content/MarkdownDisplay";
 
-import NotFound from '../NotFound';
+import NotFound from "../NotFound";
+//Todo: 试试优化代码改成只需要用一个sidebar组件, 需要配置相关路由和使用outlet
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} exact={true}>
+          <Route index element={<Home />} />
 
-
-function Router(){
-  
-    return(
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} exact={true} >
-            <Route index element={<Home />} />
-    
-            <Route path="/vision/mission" element={<Missions />}  />
-            <Route path="/vision/aboutUs/avatar" element={<Avatar />}  />
-            <Route path='/vision/aboutUs' element={<AboutUs />} />
-            <Route path='/vision/aboutUs/advisory-board' element={<Advisory />} />
-            <Route path='/vision/aboutUs/partners' element={<Partners />} />
-    
-            <Route path='/research/ongoing-projects' element={<OnGoingProjects />} />
-            <Route path='/research/finished-projects' element={<FinishedProjects />} />
-            <Route path='/research/ongoing-projects/education-development' element={<EducationDevelopment />} />
-    
-            <Route path='/publication/reports' element={<Reports />} />
-            <Route path='/publication/journal-articles' element={<JournalArticles />} />
-            <Route path='/publication/conferences' element={<Conferences />} />
-    
-            <Route path='/events/workshop' element={<Workshop />} />
-                    <Route path='/events/workshop/1' element={<WorkshopContent />} />
-            <Route path='/events/seminar' element={<Seminar />} />
-            <Route path='/events/media-cover' element={<MediaCover />} />
-                    <Route path='/events/media-cover/1' element={<MediaCoverContent />} />
-            <Route path="/joinUs" element={<JoinUs />}  />
-    
-            <Route
-              path="*"
-              element={
-                <NotFound />
-              }
-            />
+          <Route path="vision">
+            <Route path="mission" element={<Missions />} />
+            <Route path="aboutUs" element={<AboutUs />}>
+              <Route path="avatar" element={<Avatar />} />
+              <Route path="advisory-board" element={<Advisory />} />
+              <Route path="partners" element={<Partners />} />
+            </Route>
           </Route>
-        </Routes>
-      </BrowserRouter>
-    )
-}
 
+          <Route path="research">
+            <Route path="ongoing-projects" element={<OnGoingProjects />}>
+              <Route path="education-development" element={<EducationDevelopment />} />
+            </Route>
+            <Route path="finished-projects" element={<FinishedProjects />} />
+          </Route>
+
+          <Route path="publication">
+            <Route path="reports" element={<Reports />} />
+            <Route path="journal-articles" element={<JournalArticles />} />
+            <Route path="conferences" element={<Conferences />} />
+          </Route>
+
+          <Route path="events">
+            <Route path="workshop" element={<Workshop />}>
+              <Route path="1" element={<WorkshopContent />} />
+            </Route>
+            <Route path="seminar" element={<Seminar />} />
+            <Route path="media-cover" element={<MediaCover />}>
+              <Route path="1" element={<MediaCoverContent />} />
+            </Route>
+          </Route>
+
+          <Route path="joinUs" element={<JoinUs />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 export default Router;
